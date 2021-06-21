7449a02a44b59efa414ea1c9e1587f37909e60d2
Yusuf <yakhaliq01@gmail.com>
2021-06-21 15:27:38 +0100

    list bookmarks

---
+++ app.rb
@@ -0,0 +1,20 @@
+require './lib/bookmark'
+require 'sinatra/base'
+require 'sinatra/reloader'
+
+class BookmarkManager < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  get '/' do
+    'Hello World'
+  end
+
+  get '/bookmarks' do
+    @bookmarks = Bookmarks.all
+    erb :'bookmarks/index'
+  end
+
+  run! if app_file == $0
+end
---
+++ lib/bookmark.rb
@@ -0,0 +1,9 @@
+class Bookmarks
+  def self.all
+    [
+    'http://www.google.com',
+    'http://www.youtube.com',
+    'http://www.facebook.com'
+      ]
+    end
+end
